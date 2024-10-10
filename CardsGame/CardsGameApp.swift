88f1ce90e5eb1db4b7ae9e5f436f84c349be759e
Gulchin Ibrahimli <ibragimli.gulchin@gmail.com>
2024-10-10 15:35:05 +0200

    Initial Commit

---
+++ CardsGame/CardsGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardsGameApp.swift
+//  CardsGame
+//
+//  Created by Gulchin Ibragimli on 10.10.24.
+//
+
+import SwiftUI
+
+@main
+struct CardsGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
